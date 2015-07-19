67ed29cf6e0243eeb75fb73725c100258edf5e61
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-07-19 20:48:31 +0100

    Add an index route for sanity

--- src/main/scala/HelloWorldController.scala
+++ src/main/scala/HelloWorldController.scala
@@ -7,6 +7,10 @@ import com.twitter.finatra.http.Controller
 
 class HelloWorldController extends Controller {
 
+  get("/") { request: Request =>
+    response.ok.json("Welcome, please visit /hello")
+  }
+
   get("/hello") { request: Request =>
     info("I got here!")
     response.ok.json(Map("msg" -> "Hi there!"))
