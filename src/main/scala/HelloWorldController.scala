2a8c8326fcc788999026cbb8f9fbdef7d2d15036
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-07-20 00:11:52 +0100

    Use custom request. Finatra ROCKS! :dancer:

--- src/main/scala/HelloWorldController.scala
+++ src/main/scala/HelloWorldController.scala
@@ -4,6 +4,10 @@
 
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
+import com.twitter.finatra.request.QueryParam
+import com.twitter.finatra.validation.NotEmpty
+
+case class HelloRequest(@NotEmpty @QueryParam name: Option[String])
 
 class HelloWorldController extends Controller {
 
@@ -11,8 +15,9 @@ class HelloWorldController extends Controller {
     response.ok.json("Welcome, please visit /hello")
   }
 
-  get("/hello") { request: Request =>
+  get("/hello") { req: HelloRequest =>
     info("I got here!")
-    response.ok.json(Map("msg" -> "Hi there!"))
+    val name = req.name.getOrElse("Human")
+    response.ok.json(Map("msg" -> s"Hi there $name !"))
   }
 }
